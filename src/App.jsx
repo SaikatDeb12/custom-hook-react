3ce75167d6b6ab9a6b40ad27575f4c658e60c804
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-09 22:07:30 +0530

    added custom hook, usetoggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import { useToggle } from "./useToggle";
 
 function App() {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, toggle] = useToggle(false);
   return (
     <div>
-      <button
-        onClick={() => {
-          setIsVisible((prev) => !prev);
-        }}
-      >
-        {isVisible ? "Hide" : "Show"}
-      </button>
+      <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
       <h1>{isVisible ? "This is a text" : ""}</h1>
     </div>
   );
